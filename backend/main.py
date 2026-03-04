0a48ee56bf8a2a3063366e015a7bde89300fc87e
Meng Zhi Chong <56055749+Ziriki@users.noreply.github.com>
2026-03-04 21:31:22 +0800

    Complete UserDetails CRUL
    Create CRUL functions for UserDetails table.

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ import os
 import models
 from database import engine
 from controllers.user_role_controller import router as user_role_router
+from controllers.user_account_controller import router as user_account_router
+from controllers.user_details_controller import router as user_details_router
 
 # This line checks MySQL and creates the UserRole table if it doesn't exist yet!
 models.Base.metadata.create_all(bind=engine)
@@ -14,6 +16,8 @@ app = FastAPI(title="LinksLens API")
 
 # Connect the UserRole Controller to the main app
 app.include_router(user_role_router)
+app.include_router(user_account_router)
+app.include_router(user_details_router)
 
 # Data model for the mobile app to send us
 class ScanRequest(BaseModel):
