b672de788fc9cd50ed4c7f509bad49e52dcf5285
Meng Zhi Chong <56055749+Ziriki@users.noreply.github.com>
2026-03-04 21:53:55 +0800

    Complete UserPreferences CRUDL
    Create CRUDL functions for UserPreferences table.

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from database import engine
 from controllers.user_role_controller import router as user_role_router
 from controllers.user_account_controller import router as user_account_router
 from controllers.user_details_controller import router as user_details_router
+from controllers.user_preferences_controller import router as user_preferences_router
 
 # This line checks MySQL and creates the UserRole table if it doesn't exist yet!
 models.Base.metadata.create_all(bind=engine)
@@ -18,6 +19,7 @@ app = FastAPI(title="LinksLens API")
 app.include_router(user_role_router)
 app.include_router(user_account_router)
 app.include_router(user_details_router)
+app.include_router(user_preferences_router)
 
 # Data model for the mobile app to send us
 class ScanRequest(BaseModel):
