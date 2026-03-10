de91eb223654b38ab7d0a9cd0e0a30b4dd2278f2
Meng Zhi Chong <56055749+Ziriki@users.noreply.github.com>
2026-03-10 20:28:56 +0800

    Improvements on Admin Web Portal 4

--- admin/controller/rules_controller.py
+++
@@ -1,9 +0,0 @@
-# controllers/rules_controller.py
-import pandas as pd
-from models import api_client
-
-def get_rules_dataframe():
-    raw_data = api_client.fetch_url_rules()
-    if not raw_data: return pd.DataFrame()
-    df = pd.DataFrame(raw_data)
-    return df[["RuleID", "ListType", "URLDomain", "AddedBy", "CreatedAt"]]
